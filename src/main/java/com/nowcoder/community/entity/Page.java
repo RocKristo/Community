be3e5f5e68530533fa6b9fe312f69b0f9b4538f1
wang-yupeng2 <1145843677@qq.com>
2023-09-12 16:14:32 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/entity/Page.java
@@ -0,0 +1,79 @@
+package com.nowcoder.community.entity;
+
+public class Page {
+    //当前页码
+    private int current = 1;
+    //显示上限
+    private int limit = 10;
+    //数据总数（用于计算总页数）
+    private int rows;
+    //查询路径
+    private String path;
+
+    @Override
+    public String toString() {
+        return "Page{" +
+                "current=" + current +
+                ", limit=" + limit +
+                ", rows=" + rows +
+                ", path='" + path + '\'' +
+                '}';
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(int current) {
+        if(current>=1)
+            this.current = current;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        if(limit >= 1 && limit<=100)
+            this.limit = limit;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        if(rows>=0)
+            this.rows = rows;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    public int getOffset(){
+        return (current-1) * limit;
+    }
+    public int getTotal(){
+        if(rows % limit == 0){
+            return rows/limit;
+
+        }
+        else return rows/limit +1;
+    }
+    public int getFrom(){
+        int from = current-2;
+        return from<1?1:from;
+    }
+    public int getTo(){
+        int to = current+2;
+        if(to>getTotal()){
+            to = getTotal();
+        }
+        return to;
+
+    }
+}
